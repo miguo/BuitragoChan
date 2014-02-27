315c313c32724ac3aa227e94b7bf2e8e7bce8e12
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-02-27 16:30:26 +0800

    Version 1.1
    Generated photos from Instagram API
    Need to fix alignment and add more interactive features

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
 
 
-$.ajax({
-	type: "GET",
-	dataType: "jsonp",
-	cache: false,
-	url: "https://api.instagram.com/v1/tags/nofilter?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf",
-	success: function(data) {
-		for (var i = 0; i < 4; i++) {
-			$("#photoFeed").append("<div class='instaframe'>
-			<a target='_blank' href='" + data.data[i].link +"'>
-			<img src='" + data.data[i].images.standard_resolution.url +"' /></a></div>"
-		
-		}
-	}
-});
+//Function to load Instagram photos
+function loadInstaGallery() {
+	$.ajax({
+		type: "GET",
+		dataType: "jsonp",
+		cache: false,
+		url: "https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf",
+		success: function(data) {
+			for (var i = 0; i < 20; i++) {
+				$("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
+			}
+		} 
+	});
+}
 
+loadInstaGallery();
 
 
 
