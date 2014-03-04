29070c6c9dfbda82f5c3fd2ac4744677a8280382
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-03-04 23:35:30 +0800

    Version 1.2
     - Generates 12 photos at a time, center alignment is fixed.
     - View More button is currently loading 12 duplicate photos, needs to
    be fixed.
     - Grid selection colors are set but need to toggle colors upon hover.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,103 @@
 $(document).ready(function () {
 
+loadInstaGallery();
 
-//Function to load Instagram photos
+
+/********************************************************
+ * Change grid color upon hover if grid is not selected *
+ ********************************************************/
+
+$("#twoByTwo").hover(function() {
+	$(".threeByThree").css({
+		"color":"white",
+		"background-color":"#2C3539",
+		"border-color":"#2C3539",
+	});
+}, function(){
+	$(".threeByThree").css({
+		"color":"#2C3539",
+		"background-color":"white",
+		"border-color":"white",
+	});
+});
+
+
+
+/*********
+ * Setup *
+ *********/
+
+/* Variables */
+var tag = 'buitragochan';
+
+var nextLink = false;
+var loadingImages = false;
+
+/**************************************
+ * Function to load Instagram photos *
+ **************************************/
+
 function loadInstaGallery() {
 	$.ajax({
 		type: "GET",
 		dataType: "jsonp",
+		//data: {'client_id': instagram.clientID, 'max_tag_id': min},
 		cache: false,
-		url: "https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf",
-		success: function(data) {
-			for (var i = 0; i < 20; i++) {
-				$("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
-			}
-		} 
+		url: 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12',
+		success: processData
 	});
 }
 
-loadInstaGallery();
+function processData(data) {
+	for (var i = 0; i < data.data.length; i++) {
+		$("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
+		
+		$("img").hide().on("load",function(){
+    		$(this).fadeIn(1000);
+		}).each(function(){
+    	if(this.complete) $(this).trigger("load");
+		});
+	}
+}
+
+/*************
+ * View More *
+ *************/
+
+/* This function is currently loading duplicate photos, needs to be fixed */
+function loadNext() {
+  	// Prevent (redundantly) loading images if we're already loading them,
+  	// and prevent us from entering an infinite loop
+var url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12';
+var next_url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.1fb234f.5896f3fd17854bb5bca6e8bb88ec731e&max_tag_id=1382958789091';
 
 
+  	if (loadingImages || nextLink == url) {
+  	  return false;
+  	}
+  	else{
+	// We are now loading images!
+	loadingImages = true;
 
+	loadInstaGallery();
+
+    // Aaaaaand we're done loading.
+    loadingImages = false;
+
+	}
+	}
+
+/* The loadNext(); function is currently loading duplicate photos, needs to be fixed */
+/* When the user scrolls to the bottom of the page, load the next set of images */
+$("#paginate").click(function() {
+	loadNext();
 });
+
+
+
+/* Closing brackets for $(document) */
+});
+
+
+
+
