325d5e6277b2c09a7e160af7e62eb9b26f44d9ff
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-03-11 12:56:27 +0800

    Version 1.4
    Need to fix the View More button to load the next 12 photos.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,68 @@
-$(document).ready(function () {
+$(document).ready(function(){
 
-loadInstaGallery();
+    loadInstaGallery();
 
+    $("#paginate").click(function() {
+		pollInstagram();
+    });
 
-/********************************************************
- * Change grid color upon hover if grid is not selected *
- ********************************************************/
-
-$("#twoByTwo").hover(function() {
-	$(".threeByThree").css({
-		"color":"white",
-		"background-color":"#2C3539",
-		"border-color":"#2C3539",
-	});
-}, function(){
-	$(".threeByThree").css({
-		"color":"#2C3539",
-		"background-color":"white",
-		"border-color":"white",
-	});
+//end of $(document) function
 });
 
+/*****************************************************
+ * Change grid color & hover when selecting 2x2 grid *
+ *****************************************************/
 
+$("#twoByTwo").on("click", function() {
+
+	$("#twoByTwo").css({
+		"color": "white",
+    	"background-color": "#2C3539",
+    	"border-color": "#2C3539",
+	});
+
+	$("#threeByThree").css({
+		"color": "#2C3539",
+    	"background-color": "white",
+    	"border-color": "white",
+	});
+
+
+	$("img").animate({
+		"height": "420",
+		"width": "420",
+	}); 
+}); 
+
+/*****************************************************
+ * Change grid color & hover when selecting 3x3 grid *
+ *****************************************************/
+
+
+$("#threeByThree").on("click", function() {
+
+	$("#threeByThree").css({
+		"color": "white",
+    	"background-color": "#2C3539",
+    	"border-color": "#2C3539",
+	});
+
+	$("#twoByTwo").css({
+		"color": "#2C3539",
+    	"background-color": "white",
+    	"border-color": "white",
+	});
+}); 
 
 /*********
  * Setup *
  *********/
 
-/* Variables */
+//Variables for the tag to pull and min tag id
 var tag = 'buitragochan';
-
-var nextLink = false;
-var loadingImages = false;
+    min = '';
+var url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12';
+var next_url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.1fb234f.5896f3fd17854bb5bca6e8bb88ec731e&max_tag_id=1382958789091';
 
 /**************************************
  * Function to load Instagram photos *
@@ -43,12 +74,12 @@ function loadInstaGallery() {
 		dataType: "jsonp",
 		//data: {'client_id': instagram.clientID, 'max_tag_id': min},
 		cache: false,
-		url: 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12',
+		url: url,
 		success: processData
 	});
 }
 
-function processData(data) {
+function processData(data, count) {
 	for (var i = 0; i < data.data.length; i++) {
 		$("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
 		
@@ -58,45 +89,47 @@ function processData(data) {
     	if(this.complete) $(this).trigger("load");
 		});
 	}
-}
+} 
+
 
 /*************
  * View More *
  *************/
 
-/* This function is currently loading duplicate photos, needs to be fixed */
-function loadNext() {
-  	// Prevent (redundantly) loading images if we're already loading them,
-  	// and prevent us from entering an infinite loop
-var url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12';
-var next_url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.1fb234f.5896f3fd17854bb5bca6e8bb88ec731e&max_tag_id=1382958789091';
+function pollInstagram(next_url, count) {
+    $.ajax({
+        method: "GET",
+        url: next_url,
+        dataType: "jsonp",
+        jsonp: "callback",
+        jsonpCallback: "jsonpcallback",
+        success: function(data) {
+            for (var i = 0; i < data.data.length; i++) {
+                $("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
+        
+                $("img").hide().on("load",function(){
+                $(this).fadeIn(1000);
+                }).each(function(){
+                if(this.complete) $(this).trigger("load");
+                });
+            }
+            // If the next url is not null or blank:
+            if( data.pagination.next_url && count <=12 ) {
+                pollInstagram(data.pagination.next_url, ++count);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            //alert("Check your internet Connection");
+            $("#photoFeed").val($("#photoFeed").val() + 'Error\n');
+        }
+    });
+}
 
 
-  	if (loadingImages || nextLink == url) {
-  	  return false;
-  	}
-  	else{
-	// We are now loading images!
-	loadingImages = true;
-
-	loadInstaGallery();
-
-    // Aaaaaand we're done loading.
-    loadingImages = false;
-
-	}
-	}
-
-/* The loadNext(); function is currently loading duplicate photos, needs to be fixed */
-/* When the user scrolls to the bottom of the page, load the next set of images */
-$("#paginate").click(function() {
-	loadNext();
-});
 
 
 
-/* Closing brackets for $(document) */
-});
+
 
 
 
