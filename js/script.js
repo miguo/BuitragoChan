79a4b6fbf77202af8556634427c3252e0aefb9f1
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-03-11 23:03:19 +0800

    Version 1.5
     - need to center photos
     - need to load new photos in 2x2 or 3x3 depending on grid clicked

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     loadInstaGallery();
 
     $("#paginate").click(function() {
-		pollInstagram();
+		pollInstagram(next_url);
+		console.log(next_url);
     });
 
 //end of $(document) function
@@ -12,6 +13,20 @@ $(document).ready(function(){
 /*****************************************************
  * Change grid color & hover when selecting 2x2 grid *
  *****************************************************/
+/* Function for whether View More button loads photos as 2x2 or 3x3 based on grid button clicked
+function photoSize() {
+	if ($("#photoFeed").hasClass("#gridThree")) {
+        	$("img").animate({
+				"height": "306",
+				"width": "306",
+			});        
+        } else {
+        	$("img").animate({
+				"height": "420",
+				"width": "420",
+			}); 
+      	}
+} */
 
 $("#twoByTwo").on("click", function() {
 
@@ -52,6 +67,11 @@ $("#threeByThree").on("click", function() {
     	"background-color": "white",
     	"border-color": "white",
 	});
+
+	$("img").animate({
+		"height": "306",
+		"width": "306",
+	}); 
 }); 
 
 /*********
@@ -61,7 +81,7 @@ $("#threeByThree").on("click", function() {
 //Variables for the tag to pull and min tag id
 var tag = 'buitragochan';
     min = '';
-var url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf&count=12';
+var url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.5622604.0c5820e15a5643f1b4b94ab74ef45ccf';
 var next_url = 'https://api.instagram.com/v1/tags/buitragochan/media/recent?access_token=15649813.1fb234f.5896f3fd17854bb5bca6e8bb88ec731e&max_tag_id=1382958789091';
 
 /**************************************
@@ -106,15 +126,16 @@ function pollInstagram(next_url, count) {
         success: function(data) {
             for (var i = 0; i < data.data.length; i++) {
                 $("#photoFeed").append("<a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
-        
+                
                 $("img").hide().on("load",function(){
                 $(this).fadeIn(1000);
                 }).each(function(){
                 if(this.complete) $(this).trigger("load");
                 });
+
             }
             // If the next url is not null or blank:
-            if( data.pagination.next_url && count <=12 ) {
+            if( data.pagination.next_url && count <=20 ) {
                 pollInstagram(data.pagination.next_url, ++count);
             }
         },
